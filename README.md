e591e5b42bd0c4f4c8d407e481065e812cde67d6
André Luferat <contato@luferat.net>
2024-03-06 09:25:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Flutter.Annotation
+ Bloco de anotações em Flutter.
---
+++ android/app/src/main/kotlin/com/example/flutter_annotation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_annotation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
